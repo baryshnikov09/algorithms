34fdef37a6fa3e024192458a2ad877c289f74e45
Alexander <barshnikov09@yandex.ru>
2026-02-14 20:46:05 +0300

    practice number 1 v2.

--- practice1.py
+++ practice1.py
@@ -3,6 +3,7 @@ hs = {"Atlanta":["Xavier", "Yolanda", "Zeus"], "Boston":["Yolanda","Xavier", "Ze
 sh = {"Xavier":["Boston", "Atlanta", "Chicago"], "Yolanda":["Atlanta", "Boston", "Chicago"], "Zeus":["Atlanta", "Boston", "Chicago"]}
 ahs = {"Atlanta": "Xavier", "Boston": "Zeus", "Chicago":"Yolanda"}
 ash = {"Xavier":"Atlanta", "Yolanda":"Chicago", "Zeus":"Boston"}
+raspr = 'Стабильное распределение'
 t1 = perf_counter()
 for hosp, stud in hs.items():
     flag = False
@@ -12,9 +13,10 @@ for hosp, stud in hs.items():
             break
         if sh[s].index(hosp) < sh[s].index(ash[s]) and hs[hosp].index(s) < hs[hosp].index(ahs[hosp]):
             flag = True
+            raspr = 'Нестабильное распределение'
             print(f"{hosp}: {s} - неустойчивая пара")
 
 t2 = perf_counter()
 print(f"Затраченное время: {t2-t1}")
-
+print(raspr)
 
