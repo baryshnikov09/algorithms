5db047559ba05cae692e553774be9ced01cca035
Александр <programming@baryshnikov09.ru>
2026-05-02 01:36:43 +0300

    Update practice17(2).py
    correction

--- practice17(2).py
+++ practice17(2).py
@@ -1,14 +1,22 @@
-'''
-если цена падает и ri < 1 => раньше надо продавать оборудование, 
-которое падает быстрее, то есть у которого коэффициент меньше. 
-'''
-n = int(input())
-r = list(map(float, input().split()))
+import itertools
 
-items = []
-for i in range(n):
-    items.append((r[i], i + 1))  
-items.sort()  
+tests = [
+    [3/4, 1/2, 1/100],
+    [0.9, 0.5, 0.2],
+    [0.8, 0.7, 0.6],
+    [0.95, 0.4, 0.1],
+    [0.99, 0.6, 0.3, 0.05]]
 
-for coef, idx in items:
-    print(idx, end=' ')
+for r in tests:
+    print("значения", r)
+    best_order = None
+    best_sum = -1
+    for p in itertools.permutations(r):
+        s = 0
+        for t, x in enumerate(p):
+            s += 100 * x ** (t + 1)
+        if s > best_sum:
+            best_sum = s
+            best_order = p
+    print("бест порядок", best_order)
+    print("макс выручка", best_sum, "\n")
